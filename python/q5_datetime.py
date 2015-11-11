f27187d81784a77d5808878e48bed3f7d3d0768c
jmfradkin <jmfradkin@gmail.com>
2015-11-11 12:28:53 -0500

    Updated answers for Q5 (Datetime)

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,31 @@
 # Hint:  use Google to find python function
 
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+
+from datetime import datetime 
+
+date_start=datetime.strptime('01-02-2013', '%m-%d-%Y')
+date_stop=datetime.strptime('07-28-2015', '%m-%d-%Y')
+difference=date_stop-date_start
+
+print difference.days
+   
 
 ####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+
+from datetime import datetime
+
+date_start=datetime.strptime('12312013', '%m%d%Y')
+date_stop=datetime.strptime('05282015', '%m%d%Y')
+difference=date_stop-date_start
+
+print difference.days  
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+
+from datetime import datetime
+
+date_start=datetime.strptime('15-Jan-1994', '%d-%b-%Y')
+date_stop=datetime.strptime('14-Jul-2015', '%d-%b-%Y')
+difference=date_stop-date_start
+print difference.days
