17aeb1f9b8e49c3e0d86ca00e354aaaf3cab3ec5
Jamie Fradkin <jfradkin@Jamies-MacBook-Pro.local>
2015-11-18 20:19:24 -0500

    Updated code for Q8 Parsing

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,19 +1,22 @@
-#The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
-
-# The below skeleton is optional.  You can use it or you can write the script with an approach of your choice.
-
-
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+f=open('football.csv')
+csv_f = csv.reader(f)
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+teams=[]
+goals=[]
+allowed=[]
+difference=[]
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+for row in csv_f:
+	teams.append(row[0])
+	goals.append(row[5])
+	allowed.append(row[6])
+
+for i in range(1,len(goals)):
+	difference.append(abs(int(goals[i])-int(allowed[i])))
+	
+idx=difference.index(min(difference))
+print teams[idx+1]
+
+
